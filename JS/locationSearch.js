e51e043e795155825688901df19a5821dd93701e
Chase Schachenman <schachenman@wisc.edu>
2016-02-21 00:15:09 -0600

    Added grid layout for search results and filters for genres. Added conatiner for AM/FM filter. Need to get searches for AM stations too and then complete filtering based on AM/FM.

--- JS/locationSearch.js
+++ JS/locationSearch.js
@@ -1,32 +1,95 @@
+$('#lower-nav').affix({
+    offset: {top: 0}
+});
+
 window.onload = function(){
     loadResults();
+    createFilters();
 };
 
+var possibleGenres = [];
 
 function loadResults() {
-    for (var i = 1; i < 4 /**fmData.length **/; i++) {
+
+    for (var i = 1; i < fmData.length; i++) {
+
         callsign = fmData[i]['callsign'];
         toPrint = "";
+        // Check if the possible filters are added yet, if not add them to the filter array
+        if($.inArray(fmData[i]['primaryGenre'], possibleGenres) == -1) {
+            possibleGenres.push(fmData[i]['primaryGenre']);
+        }
         for (var key in fmData[i]) { // to iterate through array
             //document.write(key + ": " + fmData[i][key] + "|");
             toPrint += fmData[i][key] + "|";
+
         }
         createRow(callsign, toPrint);
     }
 }
 
 function createRow(index, toPrint){
+    // create the row
     var rowDiv = document.createElement("div");
     rowDiv.className = "row";
     rowDiv.id = index;
+    // create the column
     colDiv = document.createElement("div");
     colDiv.className = "col-md-12";
     colDiv.id = index;
-    colDiv.innerHTML = toPrint;
+    //colDiv.innerHTML = toPrint;
+    // create the search result container
+    var resultContainer = document.createElement("div");
+    resultContainer.className = "display-container";
+    resultContainer.style = "color: black";
+    resultContainer.innerHTML = toPrint;
+    // append elems.
+    colDiv.appendChild(resultContainer);
     rowDiv.appendChild(colDiv);
     var container = document.getElementById("container");
     container.appendChild(rowDiv);
 
 }
 
-// display one card first and edit formatting, then loop in rest
+function createFilters(){
+    var containerList = document.getElementById("collapseFilters");
+    for(i = 0; i < possibleGenres.length; i++){
+        // create the container for checkbox
+        var divContain = document.createElement("div");
+        divContain.className = "checkbox";
+
+        // create the checkbox
+        var checkbox = document.createElement("input");
+        checkbox.id = "checkbox" + i;
+        checkbox.type="checkbox";
+        checkbox.style="margin-left: 0;";
+
+        // create the label
+        var label = document.createElement("label");
+        label.htmlFor = "checkbox " + i;
+        label.innerHTML = possibleGenres[i];
+
+        divContain.appendChild(checkbox);
+        divContain.appendChild(label);
+
+        // finally append to container list
+        containerList.appendChild(divContain);
+
+    }
+}
+
+function changeFilterIcon(){
+    var glyph = document.getElementById("filterGlyph");
+    if(glyph.className == "glyphicon glyphicon-chevron-up pull-right")
+        glyph.className = "glyphicon glyphicon-chevron-down pull-right";
+    else
+        glyph.className = "glyphicon glyphicon-chevron-up pull-right";
+}
+
+function changeFM_AMIcon(){
+    var glyph = document.getElementById("filterGlyph2");
+    if(glyph.className == "glyphicon glyphicon-chevron-up pull-right")
+        glyph.className = "glyphicon glyphicon-chevron-down pull-right";
+    else
+        glyph.className = "glyphicon glyphicon-chevron-up pull-right";
+}
--- locationSearch.php
+++ locationSearch.php
@@ -1,7 +1,7 @@
 <?php include "templates/include/header.php"; ?>
 
 <!-- Links in the styling for displaying website result "cards" -->
-<link type="text/css" href="CSS/locationSearch.css">
+<link rel="stylesheet" href="CSS/locationSearch.css">
 <!-- first script stores the JSON data of results from server -->
 <!-- Next Script links in the necessary JavaScript file which handles displaying the results -->
 <script>fmData = JSON.parse('<?php echo json_encode($_POST['results']);?>') </script>
@@ -9,9 +9,38 @@
 
 
 <!-- Main container for results -->
-<div class="container" id="container" style="padding-top: 60px;">
+
+<div class="container-fluid" style="padding-top: 60px;">
     <div class="row">
-        <div class="col-md-12" align="center">FIRST ROW</div>
+        <div class="col-md-3">
+        <div id="sidebar" style="padding-left: 10px;" class="panel panel-default"
+             data-spy="affix" data-offset-top="60" data-offset-bottom="400">
+            <!-- Default panel contents -->
+            <div class="panel-heading"><h3>Filter Results</h3></div>
+            <div class="panel-body">
+                <span id="filterGlyph" class="glyphicon glyphicon-chevron-down pull-right" aria-hidden="true" style="margin-top: 12px;"></span>
+                <h4 data-toggle="collapse" href="#collapseFilters" onclick="changeFilterIcon()">Filter Genres</h4>
+            </div>
+            <div id="collapseFilters" class="panel-collapse collapse">
+            </div>
+            <div class="panel-body">
+                <span id="filterGlyph2" class="glyphicon glyphicon-chevron-down pull-right" aria-hidden="true" style="margin-top: 12px;"></span>
+                <h4 data-toggle="collapse" href="#collapseAM_FM" onclick="changeFM_AMIcon()">Show AM/FM</h4>
+            </div>
+            <div id="collapseAM_FM" class="panel-collapse collapse">
+
+            </div>
+        </div>
+       </div>
+        <div class="col-md-9" align="center">
+            <div class="container-fluid" id="container"
+                 style="border: 3px solid #222222;
+                 border-radius: 5px; background-color: #F5F5F5;
+                 ">
+
+            </div>
+        </div>
+
     </div>
 
 </div>
